43a50a0a15da773f17da0ab3aa8348c72b594454
Tsumari <lucasflicky@hotmail.com>
2020-06-13 21:33:38 -0300

    roulette: update assets

--- routes/generators/roulette.js
+++ routes/generators/roulette.js
@@ -4,8 +4,8 @@ const Picto = require('../../../bot/core/utilities/Picto');
 
 const staticAssets = {}
 staticAssets.load = Promise.all([
-    Picto.getCanvas('https://cdn.discordapp.com/attachments/488142183216709653/719962883525574706/unknown.png'),
-    Picto.getCanvas('https://cdn.discordapp.com/attachments/488142183216709653/719963379099369512/rubineCHIP.png'),
+    Picto.getCanvas(HOST + '/build/games/roulette/board.png'),
+    Picto.getCanvas(HOST + '/build/games/roulette/rubineCHIP.png'),
     Picto.getCanvas(HOST + '/build/games/roulette/pin.png'),
 ]).then(res=>{
     let [table,chip,pin] = res;
@@ -37,46 +37,46 @@ router.get('/', async (req,res)=>{
                     amt: parseInt(b.split('-')[1], 16)
                     , type: betTypes[parseInt(meta[0], 16)]
                     , offset: parseInt(meta[1])
-                    , number: parseInt(meta[2], 36)
+                    , number: parseInt(meta[2], 36)+1
                 }
             })
         }
     }));
 
 
-    console.log({ USERS })
-    console.log(USERS[0].bets)
+ 
+ 
 
-    const baselineGridX = i => 192 + (46 * ~~(i.number / 3));
-    const baselineGridY = i => 398 - (62 * (((i.number) % 3 || 3) - 1));
+    const baselineGridX = i => 192 + (46 * ~~(i.number / 3.1));
+    const baselineGridY = i => 405 - (62 * (((i.number) % 3 || 3) - 1));
     const typeCoords = {
-        dozen: {
-            x: _ => 750,
-            y: i => 276 + i.offset * 62
-        },
         column: {
-            x: i => i.offset == 1 ? 490 : 351,
-            y: _ => 490
+            x: _ => 750,
+            y: i => 280 + i.offset * 62
+        },
+        dozen: {
+            x: i => 250 + i.offset * 100,
+            y: _ => 464
         },
         manque: {
-            x: _ => 384,
-            y: _ => 220
+            x: _ => 380,
+            y: _ => 229
         },
         passe: {
-            x: _ => 573,
-            y: _ => 220
+            x: _ => 568,
+            y: _ => 229
         },
         colour: {
-            x: i => 192 + (i.offset == 1 ? 189 : i.offset == 2 ? 389 : 589),
-            y: _ => 500
+            x: i =>  (i.offset == 1 ? 189 : i.offset == 2 ? 389 : 589),
+            y: _ => 509
         },
         parity: {
             x: i => i.offset == 1 ? 281 : 700,
-            y: _ => 500
+            y: _ => 509
         },
         straight: {
-            x: i => i.offset ? 139 : baselineGridX(i),
-            y: i => i.offset == '2' ? 307 : i.offset == '1' ? 307 + 62 : baselineGridY(i)
+            x: i => i.offset ? 137 : baselineGridX(i),
+            y: i => i.offset == '2' ? 317 : i.offset == '1' ? 317 + 62 : baselineGridY(i)
         },
         square: {
             x: i => baselineGridX(i) + 23,
@@ -88,46 +88,103 @@ router.get('/', async (req,res)=>{
         },
         street: {
             x: i => baselineGridX(i),
-            y: _ => 430
+            y: _ => 439
+        },
+        dstreet: {
+            x: i => baselineGridX(i)+23,
+            y: _ => 260
         },
         basket: {
             x: _ => 169,
-            y: _ => 334
+            y: _ => 345
         },
         snake: {
             x: _ => 112,
-            y: _ => 470
+            y: _ => 479
         }
     }
 
+    let betsPlaced = []
     ctx.drawImage(table, 0, 0);
-    ctx.shadowColor = "#112"
+    ctx.shadowColor = "#1125"
+    const userColors = ["ff2937","00a8db","02e857","f5c105","ff7f06","690ae6","ff319f","2127d5","9cff22","1f1f23"]
+
+
     USERS.forEach((player,i) => {
 
+        console.log(player.bets)
+
         let totalBet = 0;
+        player.color = "#"+userColors[i]
 
         player.bets.forEach((bet) => {
+
+            betsPlaced.push(bet.type+bet.number+bet.offset);
+            
 
             let { x, y } = typeCoords[bet.type];
 
             let dx = x(bet)
             let dy = y(bet)
 
-            ctx.shadowBlur = 5
-            ctx.drawImage(chip, dx - 20, dy - 20, 40, 40);
+            let rpt =  betsPlaced.filter(b=>b==bet.type+bet.number+bet.offset).length-1;
+            
+            let od = -400+dx
+            let ddx = (rpt*(od/100))
+            let ddy =  -rpt*5
+
+
+
+            ctx.shadowBlur = -ddx
+            ctx.drawImage(chip, dx - 20+ddx, dy - 20+ddy, 40, 40);
+            ctx.globalCompositeOperation = 'color'
+            ctx.strokeStyle  = player.color
+            ctx.lineWidth = 6
+            ctx.beginPath();
+            ctx.arc(dx+ddx, dy+ddy, 16, 0, 2 * Math.PI);
+            ctx.stroke()
+            ctx.globalCompositeOperation = 'source-over'
+
             totalBet+= bet.amt
 
         })
-        let xpos = 300+ i % 3 * (225  + 8);
-        let ypos = 300+ ~~(i/3) * (100 + 8);
+        let xpos = 280+ i % 4 * (120  + 8);
+        let ypos = 20+ ~~(i/4) * (60 + 8);
 
-
-         
+        if(i<8){
+            ctx.strokeStyle  = player.color
+            ctx.fillStyle  = player.color+"35"
+            ctx.beginPath();
+            ctx.arc(xpos+20, ypos+20, 25, 0, 2 * Math.PI);
+            ctx.fill()
+            ctx.save();
+            ctx.beginPath();
+            ctx.arc(xpos+20, ypos+20, 20, 0, 2 * Math.PI);
+            ctx.clip();
+            ctx.drawImage(player.avatar, xpos, ypos, 40, 40);
+            ctx.restore();
+            ctx.beginPath();
+            ctx.arc(xpos+20, ypos+20, 25, 0, 2 * Math.PI);
+            ctx.lineWidth = 3
+            ctx.stroke()
             Picto.setAndDraw(ctx,
-                Picto.tag(ctx, miliarize(totalBet, 0), '600 20px "AvenirNextRoundedW01-Bold" ', '#FFF'),
-                xpos, ypos  , 128, 'center'
-            )
-         
+                Picto.tag(ctx, miliarize(totalBet, 0)+' ', '600 20px "AvenirNextRoundedW01-Bold" ', '#FFF'),
+                xpos + 55, ypos  , 128, 'left'
+            )         
+            Picto.setAndDraw(ctx,
+                Picto.tag(ctx, 'TOTAL ', '600 12px "AvenirNextRoundedW01-Regular" ', '#FFFA'),
+                xpos + 55, ypos  +20, 128, 'left'
+            )         
+
+            ctx.drawImage(chip, xpos-10, ypos+25, 20, 20);
+            ctx.globalCompositeOperation = 'color'
+            ctx.strokeStyle  = player.color
+            ctx.lineWidth = 6
+            ctx.beginPath();
+            ctx.arc(xpos, ypos+35, 8, 0, 2 * Math.PI);
+            ctx.stroke()
+            ctx.globalCompositeOperation = 'source-over'
+        }
 
     })
     
