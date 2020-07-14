b9c789bc2b6a2830cfd9fe607571fc1a775f4f46
Tsumari <lucasflicky@hotmail.com>
2020-07-14 15:20:02 -0300

    [FEAT] API: add getMarketDetails endpoint

--- routes/api.js
+++ routes/api.js
@@ -117,7 +117,7 @@ router.get('/items/:endpoint',   (req,res) => {
     if(req.params.endpoint == 'search'){
         let queries = {}
         Object.keys(req.query)
-            .filter(qry => ['id','rarity','code','type'].includes(qry) )
+            .filter(qry => ['_id','id','rarity','code','type'].includes(qry) )
             .forEach(ky=> {
                     queries[ky] = req.query[ky]
                 })
@@ -355,7 +355,7 @@ router.get('/cosmetics/:endpoint', async (req,res) => {
     if(req.params.endpoint == 'search'){
         let queries = {}
         Object.keys(req.query)
-            .filter(qry => ['id','rarity','code','icon','type'].includes(qry) )
+            .filter(qry => ['_id','id','rarity','code','icon','type'].includes(qry) )
             .forEach(ky=> {
                     queries[ky] = req.query[ky]
                 })
@@ -385,23 +385,89 @@ router.get('/achievements/:id', async (req,res) => {
     }
 
 })
+function getItemMarketDetails(item){
+    return new Promise(async (resolve,reject)=>{
 
-
+        const [cos,itm] = await Promise.all([
+            DB.cosmetics.find({_id: item }).lean().exec(),
+            DB.items.find({_id: item }).lean().exec()
+        ]).catch(async ()=> { 
+            return  await Promise.all([
+                DB.cosmetics.find({$or:[{id: item},{icon: item},{code: item}] }).lean().exec(),
+                DB.items.find({id: item }).lean().exec()
+            ]).catch(e=> { 
+                return reject(400,"Bad Request");
+            })
+        });
+        const result = cos.concat(itm)[0];
+        if(!result) return reject(404,"item not found");
+        const marketplace = await DB.marketplace.find({item_id:result._id}).lean().exec();
+        const marketplacePriceMap = marketplace.map(x=> (x.currency === 'SPH' ? 1000 : 1) * x.price );
+        
+        const response = {
+            item: result,
+            max: Math.max(...marketplacePriceMap),
+            min: Math.max(...marketplacePriceMap),
+            average: marketplacePriceMap.reduce((a,b)=> a + b,0) / marketplace.length,
+            entries: marketplace
+        }
+        return resolve(response);
+    })
+}
+router.get('/marketplace/:item', async (req,res) => {
+    const {item} = req.params;
+       
+    getItemMarketDetails(item).catch((code,reason)=>{
+        res.status(code).json({reason})
+    }).then(response=>{
+        res.json(response)
+    });
+})
 router.get('/marketplace', async (req,res) => {
  
         let queries = {}
         Object.keys(req.query)
-            .filter(qry => ['id','item_id','item_type','author','type'].includes(qry) )
+            .filter(qry => ['id','item_id','item_type','author','type','price'].includes(qry) )
             .forEach(ky=> {
                     queries[ky] = req.query[ky]
                 })
-        console.log({queries})
  
-        console.log(queries)
-        DB.marketplace.find(queries).then(result=>{
-            console.log(result)
-            res.json(result)
-        })  
+ 
+        if (req.query.after){
+
+            queries.timestamp = {$gte: Number(req.query.after) || Date.now() - 86400000 }
+        }
+        if (req.query.before){
+            queries.timestamp = {$lte: Number(req.query.before) || Date.now()  }
+        }
+
+        let lim = Math.min( Number(req.query.limit) || 25, 100 );
+        let skip = Number(req.query.skip) || 0;
+        let sort = req.query.sort == 'oldest' ? 1 : -1
+
+ 
+        DB.marketplace.find(queries, {__v:0}).sort({timestamp: sort}).limit(lim).skip(skip).then(async result=>{
+     
+            let [marketeers,cosmetics,goods]  = await Promise.all([
+                DB.users.find({id:{$in:result.map(i=>i.author)}},{meta:1,id:1}).lean().exec(),
+                DB.cosmetics.find({_id:{$in:result.map(i=>i.item_id)}}).lean().exec(),
+                DB.items.find({_id:{$in:result.map(i=>i.item_id)}}).lean().exec()
+            ]);
+ console.log(result)
+            let newThing=
+                result.map(entry=>{
+                    return Object.assign( 
+                        { itemdata: cosmetics.concat(goods).find(i=>i._id==entry.item_id)},
+                        { userdata: marketeers.find(u=>u.id===entry.author).meta},
+                        entry._doc
+                    );
+                });
+
+            res.json(newThing)
+        }).catch(e=>{
+            console.error(e)
+            res.send("What the fuck are you trying?")
+        })
 
 })
 
