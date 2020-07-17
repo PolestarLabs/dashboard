9fa0b2faee506f5f6b8371d975815577f01fc3ef
Tsumari <lucasflicky@hotmail.com>
2020-07-17 14:36:12 -0300

    update button feedback

--- public/js/plx-themes.js
+++ public/js/plx-themes.js
@@ -1,4 +1,4 @@
-// Find if user has set a preference and react to changes
+
 (function initializeTheme(){
     syncBetweenTabs()
     listenToOSChanges()
@@ -7,18 +7,17 @@
       returnThemeBasedOnOS() )
   }())
   
-  // Listen to preference changes. The event only fires in inactive tabs, so theme changes aren't applied twice.
+
   function syncBetweenTabs(){
     window.addEventListener('storage', (e) => {
       const root = document.documentElement
       if (e.key === 'plx-theme'){
         if (e.newValue === 'default') enableTheme('default', true, false)
-        else if (e.newValue === 'dark') enableTheme('dark', true, false) // The third argument makes sure the state isn't saved again.
+        else if (e.newValue === 'dark') enableTheme('noctix', true, false) 
       }
     })
   }
   
-  // Add a listener in case OS-level preference changes.
   function listenToOSChanges(){
     let mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
   
@@ -30,23 +29,21 @@
         }
       }
       else{
-        if(!root.classList.contains('theme-dark')) enableTheme('dark', true)
+        if(!root.classList.contains('theme-dark')) enableTheme('noctix', true)
       }
     })
   }
   
-  // If no preference was set, check what the OS pref is.
   function returnThemeBasedOnOS() {
     let mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
-    if (mediaQueryList.matches) return 'dark'
+    if (mediaQueryList.matches) return 'noctix'
       else {
       mediaQueryList = window.matchMedia('(prefers-color-scheme: light)')
-      if (mediaQueryList.matches) return 'borealis'
+      if (mediaQueryList.matches) return 'selenedi'
       else return undefined
       }
   }
   
-  // For subsequent page loads
   function returnThemeBasedOnLocalStorage() {
     const pref = localStorage.getItem('plx-theme')
     return pref || 'default'; 
@@ -56,4 +53,7 @@
     localStorage.setItem('plx-theme',theme)
      $('.plxtheme').remove()
      $('head').append(`<link class='plxtheme' rel='stylesheet' href='/css/themes/${theme}.css'>`)
+     $('.plx-theme-button').removeAttr('style')
+     $('.plx-theme-button.theme-'+theme).css({'opacity':1})
+     
  }
