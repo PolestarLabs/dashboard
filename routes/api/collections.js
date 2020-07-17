f50c2e2ad4d54465c83a345e05f390fdec0e5d86
Tsumari <lucasflicky@hotmail.com>
2020-07-17 14:38:42 -0300

    crafting/market api endpoints

---
+++ routes/api/collections.js
@@ -0,0 +1,238 @@
+
+
+const express = require('express');
+const router = express.Router();
+
+
+function isExact(pot,recipe){
+    return itemsMatch(pot,recipe) &&
+    recipe.every(item=>{
+        let material = pot.find(m=> m.id === (item.id||item) );
+        if (!material) return false;
+        let needs = item.count || 1;
+        let has = material.count;
+        return has >= needs
+    });
+}
+
+function itemsMatch(pot,recipe){
+    return pot.length === recipe.length &&
+    recipe.every(item=> 
+        !!pot.find(m=> 
+            m.id === (item.id || item)
+        )
+    );     
+}
+
+
+router.get('/:endpoint', cache(360),  (req,res) => {
+
+    let queries = {}
+    queries.crafted = true;
+    queries.display = true;
+
+    if(req.params.endpoint == 'search'){
+        Object.keys(req.query)
+            .filter(qry => ['_id','id','rarity','code','type'].includes(qry) )
+            .forEach(ky=> {
+                    queries[ky] = req.query[ky]
+                })
+        
+        let sort = {_id:-1}
+
+        DB.items.find(queries,{emoji:0,usefile:0,altEmoji:0})
+        .skip(parseInt(req.query.skip)||0)
+        .limit( parseInt(req.query.lim)||50)
+        .sort(sort).lean()
+        .then(result=>{
+            result.forEach(x=>{               
+                let timestamp = x._id.toString().substring(0,8)
+                x.release = parseInt( timestamp, 16 ) * 1000 
+                
+            })
+            res.json(result)
+        })
+    }else{
+        DB.items.find({id:req.params.endpoint, crafted: !0, display: !0},
+            {name:1, rarity:1, event:1, icon:1, id:1, type:1, gemcraft: 1, materials: 1, code: 1}).then(result=>{
+            console.log(result)
+            res.json(result)
+        })
+    }
+
+})
+
+
+router.post('/mix',cache(120),  async (req,res) => {
+
+    const {pot} = req.body;
+    const rars = ["C","U","R","SR","UR","XR"];
+    const potTypeMap = pot.map(i=>i.type);
+    
+    let inventory, craftingHistory;
+    if(req.user?.id){
+        let [userData,userStats] = await Promise.all([
+            DB.users.get(req.user.id, {'modules.inventory':1,'counters.crafted':1}),
+            (await DB.users.get(req.user.id, {'modules.inventory':1,'counters.crafted':1}))?.data
+        ]);
+        inventory = (userData?.modules?.inventory || []).filter(x=>x.count > 0);
+        craftingHistory = (userStats?.data?.crafted || []).map(x=>x.id);
+        
+    } 
+
+    if (!pot) return res.status(400).json({error: "No Pot"});
+    
+    const query = {
+        $or: [
+            {materials: {$all:pot.map(i=>i.id)}},
+            {'materials.id': {$all:pot.map(i=>i.id)}}
+        ],
+        //crafted: !0, display: !0
+    };
+    
+
+    const queryExact = {
+        $or: [
+            {materials: {$size: pot.length, $all: pot.map(i=>i.id) }},
+            {
+                $and:pot.map(itm=>{
+                    return {'materials.id': itm.id , 'materials.count': {$lte: itm.count} }
+                }).concat({ materials: {$size: pot.length} })
+            },
+        ],    
+        //crafted: !0, display: !0
+    };
+    let tCraftSize = ([...new Set( potTypeMap )]).length
+    console.log({tCraftSize})
+
+
+    
+    let possible =  await DB.items.find(queryExact).lean().exec();
+    console.log(possible,pot)
+    if (!possible.length) possible = await DB.items.find(query).lean().exec();
+    else possible.exact = true;
+    
+    if (!possible.length){
+        
+        const refinedPot = pot.map(item=> { item.count *= ((rars.indexOf(item.rarity)+1)/2); return item})
+        let querySameType = {
+            $and: pot.map(itm=>{
+                let craftType = itm.type;
+                let threshold = refinedPot.filter(i=>i.type === craftType ).reduce((a,b)=> ({count: a.count + b.count}))?.count || 0;
+
+                return {
+                    
+                    'typeCraft.count': { $lte: threshold },
+                    'typeCraft.type': craftType,
+    
+                }
+            })
+            .concat({'typeCraft.type': {$all: potTypeMap ,}}),
+            //crafted: !0, display: !0
+        };
+        console.log({refinedPot})
+        possible = await DB.items.find(querySameType).lean().exec();
+        const potSorted = pot.sort((a,b) => rars.indexOf(b.rarity) - rars.indexOf(a.rarity) );
+        const highestRar = potSorted[0].rarity;
+        const lowestRar = potSorted[pot.length -1].rarity;
+
+
+
+        console.table( {'length':possible.length,lowestRar,highestRar })  
+        possible = possible.filter(x=> {  
+            let isBetterThanLowest =  (rars.indexOf(x.rarity) >= (rars.indexOf(lowestRar) || 1))
+            let isSameOrInferiorThanBest = rars.indexOf(x.rarity) <= rars.indexOf(highestRar);
+            console.table({
+                thisRAR: x.rarity,lowestRar, highestRar,isBetterThanLowest,
+                isSameOrInferiorThanBest
+
+            })        
+            return isBetterThanLowest && isSameOrInferiorThanBest
+        });
+        console.log('length',possible.length)  
+        
+        
+        if (possible.length){            
+            possible = [shuffle(possible)[0]]
+            possible.typeCraft = true
+        }else{
+            querySameType = {'typeCraft.type' : {$all: potTypeMap } }
+            if (pot.length === 3) querySameType.rarity = {$in: pot.map(i=>i.rarity)};
+            console.log(querySameType)        
+            possible = await DB.items.find(querySameType).lean().exec();
+            possible.typeCraft = true
+            possible.notQuite = true
+        }
+  
+    } 
+        
+
+
+    
+    if (possible.exact && possible.length > 1){
+        
+        let possiblest = possible.sort((a,b)=>{
+            //if (craftingHistory.includes(a.id)) return -1;
+            //if (inventory.map(x=>x.id).includes(a.id)) return -1;
+            if ( a.materials.every(x=> x.count <= pot.find(y=> y.id == x.id)?.count ) ) return -1;
+            if ( rars.indexOf(a.rarity) < rars.indexOf(b.rarity) ) return -1;
+            if ( rars.indexOf(a.rarity) > rars.indexOf(b.rarity) ) return  1;
+            if ( a.level < b.level  ) return  -1;
+            if ( a.level > b.level  ) return   1;
+        })
+        possible = [possiblest[0]]
+        possible.exact = true;
+    }
+
+    
+
+    if (possible.typeCraft && !possible.notQuite){
+
+        let discovery   = possible[0];
+        
+        let canCraftNow = !possible.notQuite;
+        let isDiscovery = true;
+
+        return res.json({discovery, isDiscovery, canCraftNow, typeCraft: true});
+        
+    }
+    
+
+    if (possible.exact){
+
+        let discovery = possible[0];
+        let canCraftNow = isExact(pot,discovery.materials);
+        let isDiscovery = itemsMatch(pot, discovery.materials);
+
+        return res.json({discovery, isDiscovery, canCraftNow});
+        
+    }else{
+        return res.json({
+            possible: possible.length ||0, 
+            inventory
+            
+        })
+    };
+
+
+
+
+
+   
+
+
+    
+    
+    DB.items.find({$or: [{materials: {$all:items}}, {'materials.id': {$all:items}}], crafted: !0, display: !0},
+        {name:1, rarity:1, event:1, icon:1, id:1, type:1, gemcraft: 1, materials: 1, code: 1}).then(result=>{
+        
+            result.map(itm=> {
+                itm.materials.every(t=>items.includes(t))
+            })
+        res.json(result)
+    })
+     
+})
+
+
+module.exports = router
