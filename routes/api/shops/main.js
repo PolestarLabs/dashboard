590c1bcd434c47ddd6b2521ac9e8d5b5632f7aae
Tsumari <lucasflicky@hotmail.com>
2020-07-22 07:36:23 -0300

    buy bg medal and [wip]bundle

---
+++ routes/api/shops/main.js
@@ -0,0 +1,152 @@
+const ECO = require("../../../pipelines/economy.js");
+const express = require("express");
+const router = express.Router();
+const { bgPrices, medalPrices,sapphireModifier, jadeModifier, tokenModifier } = require("../../../../bot/GlobalNumbers.js");
+const { query } = require("express");
+const defaultPrices = {
+    background: bgPrices,
+    bundle: bgPrices * 10,
+    medal: medalPrices
+}
+
+router.get("/", cache(60), async (req,res)=>{
+
+})
+
+ 
+
+router.post("/:type/buy/:finder", async (req,res)=>{
+
+    const {type,finder} = req.params;
+    const {currency} = req.body;
+
+    if (currency === 'JDE') return res.status(401).json({status:"Jades are no longer supported as currency"});
+
+    const userData = await DB.users.findOne({id: req.user.id});
+
+    let item = await DB.cosmetics.findOne({ 
+        type,
+        $or: [{id: finder},{icon: finder},{code: finder}]        
+    }).lean();
+
+    const price = ~~(itemPrice(item,currency));
+    
+    if(!item) return res.status(404).json({status: "ITEM NOT FOUND"});
+    if(!userData) return res.status(404).json({status: "USER NOT FOUND"});
+    
+    if (type === 'bundle'){
+        return buyBundle({req,res,userData,bundle:item,price,currency});
+    }
+
+    if(
+        (type == 'medal' && userData.modules.medalInventory.includes(item.icon)) ||
+        (type == 'background' && userData.modules.bgInventory.includes(item.code))
+    )  return res.status(403).json({status: "ITEM ALREADY IN INVENTORY"});
+     
+
+    if ( !(await ECO.checkFunds(userData.id,price,currency)) )  return res.status(403).json({status:"Insufficient Funds"});
+    let trans = await ECO.pay(userData.id,price,`Webshop - ${type}`,currency).catch(err=>{
+        res.status(500).json({status: "ERROR:ECOFAIL"});
+    });
+
+    switch(type){
+        case "background":
+            userData.update({$addToSet: {'modules.bgInventory': item.code} });
+            res.status(200).json({status:"OK", cost: price, trans});
+            break;
+
+        case "medal":
+            userData.update({$addToSet: {'modules.medalInventory': item.icon} });
+            res.status(200).json({status:"OK", cost: price, trans});
+            break;
+
+        default:
+            res.status(500).json({status: "ERROR:NOTYPE"});
+    }
+
+})
+
+async function buyBundle({req,res,userData,bundle,price,currency}){
+
+    let parseBundle = calculateBundlePrice({userData,bundle,price});
+
+    return res.json({payload})
+};
+
+async function calculateBundlePrice({userData,bundle,price}){
+
+    let tally = 0;
+    let owned = []
+    let toBeAdded = []
+    let query = {$addToSet:{
+        "modules.bgInventory" : {$each:[]},
+        "modules.medalInventory" : {$each:[]},
+        "modules.stickerInventory" : {$each:[]},
+    }};
+
+    price = price || bundle.price;
+
+    bundle.items.forEach(itm=>{
+        let ownsItem = false;
+        switch(itm.type){
+            case "background":
+            case "bg":
+                if(userData.modules.bgInventory.includes(itm.id)){
+                    ownsItem = !0
+                    tally+= 1;
+                }else{
+                    query.$addToSet["modules.bgInventory"].$each.push(itm.id)
+                }
+                break;
+            case "medal":
+                if(userData.modules.medalInventory.includes(itm.id)){
+                    ownsItem = !0
+                    tally+= .6;
+                }else{
+                    query.$addToSet["modules.medalInventory"].$each.push(itm.id)
+                }
+                break;
+            case "sticker":
+                if(userData.modules.stickerInventory.includes(itm.id)){
+                    ownsItem = !0
+                    tally+=1.3;
+                }else{
+                    query.$addToSet["modules.stickerInventory"].$each.push(itm.id)
+                }
+                break;
+        }
+        ownsItem ? owned.push(itm) : toBeAdded.push(itm);
+    })
+ 
+    let adjustment = tally / bundle.items.length;
+    let finalPrice = Math.max(0,~~(price - price * adjustment));
+
+    const payload = {
+        finalPrice,
+        originalPrice: price,
+        discount: Math.min(~~(adjustment*100),100)+"%",
+        complete: !toBeAdded.length,
+        owned,toBeAdded,query
+        
+    }
+
+    return  payload 
+};
+
+
+function itemPrice (item,currency="RBN"){
+    let basePrice = item.price || defaultPrices[item.type]?.[item.rarity]
+    switch (currency){
+        case "SPH":
+            return basePrice * sapphireModifier;
+        case "JDE":
+            return basePrice * jadeModifier;
+        case "EVT":
+            return basePrice * tokenModifier;
+        case "RBN":
+        default:
+            return basePrice;
+    }
+}
+
+module.exports = router;
