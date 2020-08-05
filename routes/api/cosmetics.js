436acee85cc862dd8eae24b9313695c74fdd9f94
Tsumari <lucasflicky@hotmail.com>
2020-08-05 06:16:37 -0300

    advanced sticker data on api fetch

--- routes/api/cosmetics.js
+++ routes/api/cosmetics.js
@@ -21,19 +21,38 @@ router.get('/:endpoint', cache(2660), async (req,res) => {
         .skip(parseInt(req.query.skip)||0)
         .limit( parseInt(req.query.lim)||50)
         .sort(sort).lean()
-        .then(result=>{
-            console.log(result)
+        .then(async result=>{
+            if(result && req.query.type === 'sticker'){
+                let packs = await DB.items.find({icon: {$in:result.map(x=>x.series_id)}}).lean();
+                await Promise.all(packs.map(stickerCount));
+                result.forEach(x=>{               
+                    x.packData = packs.find(y=> y.icon === x.series_id ) 
+                })
+            }
+
             result.forEach(x=>{               
                 let timestamp = x._id.toString().substring(0,8)
                 if(x.event == 'none' ) x.event = false;
                 x.release = parseInt( timestamp, 16 ) * 1000 
             })
-            console.log(result.length)
+            console.log(result)
             res.json(result)
         })
     }
 
 })
 
+
+
+async function stickerCount(pack){
+    let [pdata,mdata] = await Promise.all([
+        DB.cosmetics.find({series_id:pack.icon},{name:1,id:1,rarity:1}).lean(),
+        DB.items.find({id: {$in:pack.materials.map(x=>x.id||x) } },{name:1,id:1,rarity:1}).lean()
+    ]);
+    pack.materialsData = mdata;
+    pack.size = pdata.length;
+    pack.content = pdata;
+    return pack;
+}
 
 module.exports = router
