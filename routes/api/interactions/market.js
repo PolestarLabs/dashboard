131bbc64b3bb06fe95d1d3b5e8c6730fc8220065
Tsumari <lucasflicky@hotmail.com>
2020-12-16 14:19:38 -0300

    draft: market demo

--- routes/api/interactions/market.js
+++ routes/api/interactions/market.js
@@ -1,18 +1,18 @@
 let CURRCHOICE = {   
         name:"currency",
-        description: "The currency to be used",
+        description: "💵 The currency to be used",
         type: 3,
         choices:[
-            {name: "Rubines", value: "RBN"},
+            {name: "Rubines", value: "RBN", default: true},
             {name: "Sapphires", value: "SPH"},
         ]
     }
-
-let ITEMCHOICE = {   
+    
+    let ITEMCHOICE = {   
         name:"item",
-        description: "the item to sell",
-        type: 3,
-  
+        required: true,
+        description: "⚱️ The item to be posted",
+        type: 3,  
     }
 
 
@@ -24,24 +24,26 @@ module.exports = {
 
         {
             name: "post",
-            description: "Creates a new listing",
+            description: " Creates a new listing",
             type: 2,
             options:[
                 {
                     name: "sell",
-                    description: "Post an item for sale",
+                    description: "🛍️ Post an item for sale",
                     type: 1,
                     options:[
-                        CURRCHOICE
+                        ITEMCHOICE,
+                        CURRCHOICE,
                     ]
 
                 },
                 {
                     name: "buy",
-                    description: "Post an offer to buy an item",
+                    description: "🛍️ Post an offer to buy an item",
                     type: 1,
                     options:[
-                        CURRCHOICE
+                        ITEMCHOICE,
+                        CURRCHOICE,
                     ]
 
                     
@@ -53,36 +55,56 @@ module.exports = {
         {
             name: "list",
             description: "List Entries",
-            type: 1,
-            oprtion: [
+            type: 2,
+            default: true,
+            options: [
                 {
-                    name: "self",
-                    description: "List your own entries",
-                    type: 3,
-                    value: 
+                    name: "Self",
+                    description: "🧾 List your own entries",
+                    type: 1,
+                    default: true,
+                    value: "SELF"
 
                 },
                 {
-                    name: "user",
-                    description: "List entries from a user.",
-                    type: 6,
+                    name: "Player",
+                    description: "📕 List entries from a player.",
+                    type: 1,
+                    options: [
+                        {
+                            name: "player",
+                            description: "The player to be searched.",
+                            type: 6,
+                            required: true
+                        }
+                    ]
 
                 },
                 {
-                    name: "item",
-                    description: "The item you want to search.",
-                    type: 3,
+                    name: "Item",
+                    description: "⚱️ The item you want to search.",
+                    type: 1,
+                    options: [
+                        {
+                            name: "item",
+                            description: "The item name or ID.",
+                            type: 3,
+                            required: true
+                        }
+                    ]
+                    
 
                 },
             ]
         },
         {
             name: "remove",
-            description: "Remove Entries",
+            description: "♻️ Remove Entries",
             type: 1,
             options: [
                 {
                     name: "entry",
+                    required: true,
                     description: "The entry ID of the listing you wanna remove.",
                     type: 3
                 }
