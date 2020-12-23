d61b103f4eb87a19932138cee2f4ff29e4ba0ab8
Mitchell <contact@mitchells.work>
2020-12-23 03:02:02 +0100

    run things through gulp pipeline

---
+++ gulpfile.js
@@ -0,0 +1,163 @@
+const { join } = require("path");
+const { parallel, series, src, dest } = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const image = require("gulp-image");
+const debug = require("gulp-debug");
+const minify = require("gulp-minify");
+
+const ignore = ["!./**/*OLD*/*.*", "!./**/*OLD*.*"];
+
+const outDir = "dist";
+const out = (file) => file.base.replace("views", join(outDir, "views")).replace("public", join(outDir, "public"));
+
+function css() {
+    return src(["./public/**/*.css", "./views/**/*.css", ...ignore])
+        .pipe(debug())
+        .pipe(cleanCSS())
+        .pipe(dest(out));
+}
+
+function js() {
+    return src(["./public/**/*.js", "./views/**/*.js", ...ignore])
+        .pipe(debug())
+        .pipe(minify())
+        .pipe(dest(out));
+}
+
+function assets() {
+    return src([`./public/**/*.{${imgExt}}`, `./views/**/*.{${imgExt}}`, ...ignore])
+        .pipe(image())
+        .pipe(dest(out));
+}
+
+function others() {
+    return src(["./public/**/*.*", "./views/**/*.*", `!./**/*.{css,js,${imgExt}}`, ...ignore])
+        .pipe(debug())
+        .pipe(dest(out));
+}
+
+exports.build = series(css, js, assets, others);
+
+const imgExt = [
+	"ase",
+	"art",
+	"bmp",
+	"blp",
+	"cd5",
+	"cit",
+	"cpt",
+	"cr2",
+	"cut",
+	"dds",
+	"dib",
+	"djvu",
+	"egt",
+	"exif",
+	"gif",
+	"gpl",
+	"grf",
+	"icns",
+	"ico",
+	"iff",
+	"jng",
+	"jpeg",
+	"jpg",
+	"jfif",
+	"jp2",
+	"jps",
+	"lbm",
+	"max",
+	"miff",
+	"mng",
+	"msp",
+	"nitf",
+	"ota",
+	"pbm",
+	"pc1",
+	"pc2",
+	"pc3",
+	"pcf",
+	"pcx",
+	"pdn",
+	"pgm",
+	"PI1",
+	"PI2",
+	"PI3",
+	"pict",
+	"pct",
+	"pnm",
+	"pns",
+	"ppm",
+	"psb",
+	"psd",
+	"pdd",
+	"psp",
+	"px",
+	"pxm",
+	"pxr",
+	"qfx",
+	"raw",
+	"rle",
+	"sct",
+	"sgi",
+	"rgb",
+	"int",
+	"bw",
+	"tga",
+	"tiff",
+	"tif",
+	"vtf",
+	"xbm",
+	"xcf",
+	"xpm",
+	"3dv",
+	"amf",
+	"ai",
+	"awg",
+	"cgm",
+	"cdr",
+	"cmx",
+	"dxf",
+	"e2d",
+	"egt",
+	"eps",
+	"fs",
+	"gbr",
+	"odg",
+	"svg",
+	"stl",
+	"vrml",
+	"x3d",
+	"sxd",
+	"v2d",
+	"vnd",
+	"wmf",
+	"emf",
+	"art",
+	"xar",
+	"png",
+    "webp",
+    "webm",
+	"jxr",
+	"hdp",
+	"wdp",
+	"cur",
+	"ecw",
+	"iff",
+	"lbm",
+	"liff",
+	"nrrd",
+	"pam",
+	"pcx",
+	"pgf",
+	"sgi",
+	"rgb",
+	"rgba",
+	"bw",
+	"int",
+	"inta",
+	"sid",
+	"ras",
+	"sun",
+	"tga"
+].join(",");
