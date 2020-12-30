1ebe9b47c9ddc3328fe157b09c50cf21bb2fde39
Tsumari <lucasflicky@hotmail.com>
2020-12-30 15:54:34 -0300

    add install() (invite) utility function

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -3,4 +3,9 @@ function login() {
     window.open('/auth', 'Login', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=400,height=500")
   
   }
+  function install() {
+
+    window.open('/invite', 'Hire Pollux', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
+  
+  }
   
