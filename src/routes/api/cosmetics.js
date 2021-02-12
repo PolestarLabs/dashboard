53e2635d1521056646855219d859e1fd4e45b357
Tsumari <lucasflicky@hotmail.com>
2021-02-12 08:45:01 -0300

    cosmetics cleanup

--- src/routes/api/cosmetics.js
+++ src/routes/api/cosmetics.js
@@ -3,6 +3,28 @@
 const express = require('express');
 const router = express.Router();
 
+function CLEANUP(item) {
+    if(item.event == 'none' ) item.event = false;
+    return {
+        id: item._id,
+        legacy_id: item.id,
+        legacy_code: item.code,
+        legacy_icon: item.icon,
+        rarity: item.rarity,
+        credit:{
+            artist_name: item.artistName,
+            artist_url: item.artistLink,
+            artist_avatar: item.artistLink,
+        },
+        type: item.type,
+        catalog: item.GROUP,
+        bundle_info: item.BUNDLE,
+        is_event: !!item.event,
+        event_id: item.event ? item.event : undefined,
+        release: new Date(parseInt( item._id.toString().substring(0,8), 16) * 1000)
+    }
+}
+
 
 router.get("/search", cache(2600), async (req,res) =>{
     let queries = {}
@@ -37,6 +59,30 @@ router.get("/search", cache(2600), async (req,res) =>{
         res.json(result)
     })
 })
+
+router.get("/backgrounds/:id", cache(0.01), async (req,res) =>{
+
+    DB.cosmetics.find({type:"background",id:req.params.id},{public:0,meta:0})
+        .skip(parseInt(req.query.skip)||0)
+        .limit( parseInt(req.query.lim)||50)
+        .lean()
+        .then(async result=>{            
+            res.json( result.map(CLEANUP) );
+        })
+})
+
+router.get("/medals/:id", cache(2600), async (req,res) =>{   
+    DB.cosmetics.find({type:"medal",id:req.params.id},{public:0,meta:0})
+        .skip(parseInt(req.query.skip)||0)
+        .limit( parseInt(req.query.lim)||50)
+        .lean()
+        .then(async result=>{
+            res.json( result.map(CLEANUP) );
+        })
+})
+
+
+
 
 router.get("/count/:type", cache(360000), async (req,res) =>{
     const {type} = req.params;
