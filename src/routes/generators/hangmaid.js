0b871149df7b0ddb29f7b5cf703518cb89aeca95
Tsumari <lucasflicky@hotmail.com>
2021-02-20 16:02:10 -0300

    remove api endpoint from generators

--- src/routes/generators/hangmaid.js
+++ src/routes/generators/hangmaid.js
@@ -117,18 +117,5 @@ router.get('/', async (req,res)=>{
     canvas.pngStream().pipe(res);
 })
 
-router.get('/words', (req, res) => {
-    const quantity = req.params.q
-    if (quantity) {
-        const words = require('../api/words.json')
-        res.json(shuffle(words).slice(0, quantity)).writeHead(200, {
-            'Content-Type': 'application/json'
-        })
-    } else {
-        res.json(words).writeHead(200, {
-            'Content-Type': 'application/json'
-        })
-    }
-})
 
 module.exports = router
