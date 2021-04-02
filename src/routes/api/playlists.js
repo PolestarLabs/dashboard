be2fb0e66a62ecc198dd613ce717ea9d6f37e70b
Tsumari <lucasflicky@hotmail.com>
2021-04-02 03:29:55 -0300

    introduce playlists api endpoint

---
+++ src/routes/api/playlists.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+//DB.usercols
+router.get('/:userID', async (req,res)=>{
+    const {userID} = req.params;
+    const userCollection = await DB.usercols.get(userID);
+    if (!userCollection) return res.status(404).json( "USER NOT FOUND");
+    const {collections} = userCollection;
+    return res.status(200).json( collections.playlist || [] );    
+})
+
+router.post('/:userID', async (req,res)=>{
+    const {userID} = req.params;
+    const track = req.body;
+    if (!track) return res.status(400).json( "BAD REQUEST: NO BODY");
+    await DB.usercols.set(userID, {$addToSet: {"collections.playlist": track} });
+    return res.status(200).json( "OK" );    
+})
+
+router.delete('/:userID', async (req,res)=>{
+    const {userID} = req.params;
+    const track = req.body.url;
+    if (!track) return res.status(400).json( "BAD REQUEST: NO BODY");
+    await DB.usercols.set( {id:userID,'collections.playlist.url':track}, {$set: {"collections.playlist.$": 1} });
+    await DB.usercols.set( userID, {$pull: {"collections.playlist": 1} });
+    return res.status(200).json( "OK" );    
+})
+
+
+module.exports = router
+
