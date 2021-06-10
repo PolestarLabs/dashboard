13f1e9ebd5acfe5323cd4b0e0d0994620d79ac59
Tsumari <lucasflicky@hotmail.com>
2021-06-10 15:09:48 -0300

    switch hard beta to false

--- src/routes/paypal.js
+++ src/routes/paypal.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const beta = true; 
+const beta = false; 
 
 const request = require('request');
 const cfg = require('../../config.js');
