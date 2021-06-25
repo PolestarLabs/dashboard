121661afad2c117d03d8a3781ef1d9e9efa3a456
Tsumari <lucasflicky@hotmail.com>
2021-06-24 21:56:57 -0300

    prime invite gating process

--- src/routes/invite.js
+++ src/routes/invite.js
@@ -5,45 +5,76 @@ const axios = require("axios");
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.redirect(
-      `https://discord.com/api/oauth2/authorize`+
-        `?client_id=${ PLX.id }&permissions=2147532800`+
-        //`&redirect_uri=${ encodeURIComponent(HOST+"/invite/newserver") }`+
-        `&redirect_uri=${ "https://hijola.pollux.gg/invite/newserver" }`+
-        `&response_type=code`+
-        `&scope=${["applications.commands","bot","identify","guilds","connections","email"].join("%20")}`+
-        `&permissions=268492816`+
-        `&disable_guild_select=${ !!req.query.sv }`+
-        `&guild_id=${ req.query.sv }`)
-  });
+const inviteString = (selected_client,server) => 
+    `https://discord.com/api/oauth2/authorize`+
+    `?client_id=${ selected_client }&permissions=2147532800`+
+    `&redirect_uri=${ HOST+"/invite/verify/" + selected_client }`+      
+    `&response_type=code`+
+    `&scope=${["applications.commands","bot","identify","guilds","connections","email"].join("%20")}`+
+    `&permissions=268492816`+
+    `&disable_guild_select=${ !!server }`+
+    `&guild_id=${ server }`;
+  
 
-  router.get('/newserver', (req,res) => {
 
-    const options = {
-    method: 'POST',
-    url: 'https://discord.com/api/v9/oauth2/token',
+  
+
+router.get("/verify/:flavor", (req, res) => {
+  const { flavor } = req.params;
+  const { guild_id } = req.query;
+  const selected_client = config.clients.find((c) => c.id === flavor);
+
+  if (selected_client.category === "premium"){
+    const UID = req.user.id;
+    if (!UID) return res.status(403).redirect("/auth");
+    const userData = DB.users.get(UID);
+
+    if (!userData.prime?.servers?.includes(guild_id)){
+      return res.status(403).send("PRIME NOT ENABLED FOR THIS SERVER");
+    }
+  }
+
+
+  const options = {
+    method: "POST",
+    url: "https://discord.com/api/v9/oauth2/token",
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     data: new URLSearchParams({
-            client_id: '354285599588483082',
-            client_secret: '',
-            grant_type: 'authorization_code',
-            code: req.query.code,
-            redirect_uri: 'https://hijola.pollux.gg/invite/newserver'
-        })
-    };
+      client_id: selected_client.id,
+      client_secret: selected_client.secret,
+      grant_type: "authorization_code",
+      code: req.query.code,
+      redirect_uri: HOST + "/invite/verify/" + selected_client.id,
+    }),
+  };
 
-    axios.request(options).then(function (response) {
-        return res.status(200).json({q:req.query,data:response.data});
-    }).catch( async function (error) {
-        console.log(error.response.data);
-        return res.status(500).json({r: error.response.data  });
-        
+  axios
+    .request(options)
+    .then(function (response) {
+      return res.redirect("/setup/"+guild_id)
+    })
+    .catch(async function (error) {
+      console.log(error.response.data);
+      return res.status(500).json({ r: error.response.data });
     });
-        
-    
-  });
+});
+
+router.get('/:flavor', function (req, res) {
+  const {flavor} = req.params;
+  const {sv:server} = req.query;
+
+  const flavored_client = config.clients.find(c=> c.name === flavor) || config.clients.find(c=> c.id === PLX.id);
+  
+  console.log({flavored_client})
+
+  res.redirect( inviteString(flavored_client.id, server) );
+})
+router.get('/', function (req, res) {
+  const {sv:server} = req.query;  
+  const flavored_client = config.clients.find(c=> c.id === PLX.id);  
+  res.redirect( inviteString(flavored_client.id, server) );
+})
   
 module.exports = router
