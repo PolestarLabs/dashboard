fbf08327a0741fd84739a7751f1f08b260354f3e
Tsumari <lucasflicky@hotmail.com>
2021-07-13 18:32:58 -0300

    prime settings dashboard

---
+++ src/views/dashboard/pages/prime.js
@@ -0,0 +1,45 @@
+const PRIME = new Vue({
+    el: "#primedata",
+    data:{
+        servers: [],
+        users: [],
+        userPrimeStatus: (userdata||{}).prime,
+        serverToPrime: {name:"Select a server..."},
+        primable_servers: userinfo.servers.filter(g=> g.owner || (g.permissions & 0x8) > 0 || (g.permissions & 0x20) > 0 ),
+    },
+    components: {
+        "v-select": VueSelect.VueSelect,
+      },
+    methods: {
+        getServer(svid){
+            fetch("/api/server/"+svid).then(res=>{
+                if (res.status !== 200) return;
+                res.json().then(res=>{
+                    this.servers.push(res);
+                    PRIME.getUser(res.clientID);
+                    (res.activeClients||[]).forEach(cli=> PRIME.getUser(cli));
+                    this.servers.loading = false;
+                })
+            })
+        },
+        getUser(uid){
+            fetch("/api/user/"+uid).then(res=>{
+                if (res.status !== 200) return;
+                res.json().then(res=>{
+                    if (this.users.find(u => u.id === res.id)){
+
+                    }else{
+                        this.users.push(res);
+                    }                    
+                    this.users.loading = false;
+                })
+            })
+        }
+    }
+});
+if(userdata && userdata.prime && userdata.prime.servers){
+    userdata.prime.servers.forEach(sv=>{
+        PRIME.getServer(sv);
+    });
+}
+
