68353a964f265a398d311133431063f5ae3ca15b
Tsumari <lucasflicky@hotmail.com>
2021-07-13 11:26:03 -0300

    user cache time down to 10m

--- src/neodash.js
+++ src/neodash.js
@@ -194,7 +194,7 @@ redis:{
       if (!val) return;
       userCacheMap.set(k,val);
       PLX.redis.set("discord.users."+k,JSON.stringify(val));
-      PLX.redis.expire("discord.users."+k, 30 * 60 * 1);
+      PLX.redis.expire("discord.users."+k, 10 * 60 * 1);
     },
     get(k){
       if (userCacheMap.get(k)) {
