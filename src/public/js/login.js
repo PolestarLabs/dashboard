5eb29225602c57dbe79c05c7b1cd8560acb69fcb
Tsumari <lucasflicky@hotmail.com>
2021-07-14 00:35:39 -0300

    pass server ID to flavored invite

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -9,9 +9,9 @@ function login() {
   
   }
   
-  function installFlavored(flavor) {
+  function installFlavored(flavor,server) {
 
-    window.open(`/invite/${flavor}`, 'Activate Pollux Prime', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
+    window.open(`/invite/${flavor}?sv=${server}`, 'Activate Pollux Prime', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
   
   }
   
