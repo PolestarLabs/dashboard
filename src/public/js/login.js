a4a6774649900e5a17f5fd9334deb763c0b75b96
Tsumari <lucasflicky@hotmail.com>
2021-07-14 00:32:22 -0300

    flavored prime client install dialog

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -9,3 +9,9 @@ function login() {
   
   }
   
+  function installFlavored(flavor) {
+
+    window.open(`/invite/${flavor}`, 'Activate Pollux Prime', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
+  
+  }
+  
