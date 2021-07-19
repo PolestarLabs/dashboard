fe7172c5b0a0bfd5a27cffcf6e3d508ae50b9b76
Tsumari <lucasflicky@hotmail.com>
2021-07-19 02:06:19 -0300

    include activation endpoint in install action

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -9,10 +9,12 @@ function login() {
   
   }
   
-  function installFlavored(flavor,server) {
-
-    window.open(`/invite/${flavor}?sv=${server}`, 'Activate Pollux Prime', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
-  
+  function installFlavored(flavor,server,activate=false) {
+    if (activate){
+      window.open(`/invite/activate/${flavor}/${server}`, 'Activate Pollux Prime', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
+    }else{
+      window.open(`/invite/${flavor}?sv=${server}`, 'Invite Pollux Prime', "directories=0,titlebar=0,toolbar=0,location=false,status=0,menubar=0,scrollbars=no,resizable=no,width=1280,height=720")
+    }
   }
   function authwindow(url,text="Login") {
 
