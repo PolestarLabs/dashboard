33fdb84b2e61f655bbee03cbc568f1c0624d3294
Tsumari <lucasflicky@hotmail.com>
2021-08-06 18:24:39 -0300

    dash instrumentation 1
    (cherry picked from commit 6165edb1203a786cab3b39ffddab2feaefa103e1)

--- src/neodash.js
+++ src/neodash.js
@@ -445,6 +445,7 @@ const authCacheExpiration = new Map();
 
 app.use(function(req,res,next){
 	res.locals.HOST = HOST;
+	res.locals.ENV = process.env.NODE_ENV;
 	res.locals.INSTANCE_VUE_PATH = process.env.NODE_ENV === "production" 
 		? "https://cdn.jsdelivr.net/npm/vue@2.6.14"
 		: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.js";
