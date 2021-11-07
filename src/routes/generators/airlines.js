55cd3f1231a54949be95459b0e09e21c60a729e5
Tsumari <lucasflicky@hotmail.com>
2021-11-07 09:50:56 -0300

    airlines cleanup

--- src/routes/generators/airlines.js
+++ src/routes/generators/airlines.js
@@ -6,42 +6,41 @@ const router = express.Router();
 
 router.get('/generic-logo.png', async (req,res)=>{
 	
-	const canvas = Picto.new(128,128);
-	const canvas2 = Picto.new(128,128);
-	const ctx = canvas.getContext('2d');
-	const ctx2 = canvas2.getContext('2d');
+	const canvas_bg = Picto.new(128,128),
+	 		canvas_sw = Picto.new(128,128);
+	const ctx_bg = canvas_bg.getContext('2d');
+	const ctx_sw = canvas_sw.getContext('2d');
 	
-	const base = await Picto.getCanvas(HOST + "/build/airlines/generic_base.png");
-	const swoosh = await Picto.getCanvas(HOST + "/build/airlines/generic_swoosh.png");
-
+	const [base,swoosh] = await Promise.all([
+		Picto.getCanvas(HOST + "/build/airlines/generic_base.png"),
+		Picto.getCanvas(HOST + "/build/airlines/generic_swoosh.png")
+	]);
+	
 	const {name,c1,c2} = req.query;
+	
+	ctx_bg.fillStyle = "#" + (c1 || "00F");
+	ctx_bg.fillRect(0,0,128,128);
+	ctx_bg.globalCompositeOperation = 'destination-in';
+	ctx_bg.drawImage(base,0,0);
 
-	console.log({c1,c2})
+	ctx_sw.fillStyle = "#" + (c2 || "0FF");
+	ctx_sw.fillRect(0,0,128,128);
+	ctx_sw.globalCompositeOperation = 'destination-in';
+	ctx_sw.drawImage(swoosh,0,0);
 
 	
-	ctx.fillStyle = "#" + (c1 || "00F");
-	ctx.fillRect(0,0,128,128);
-	ctx.globalCompositeOperation = 'destination-in';
-	ctx.drawImage(base,0,0);
-
-	ctx2.fillStyle = "#" + (c2 || "0FF");
-	ctx2.fillRect(0,0,128,128);
-	ctx2.globalCompositeOperation = 'destination-in';
-	ctx2.drawImage(swoosh,0,0);
-
-	
-	ctx.globalCompositeOperation = 'source-over';
-	ctx.drawImage(canvas2,0,0);
+	ctx_bg.globalCompositeOperation = 'source-over';
+	ctx_bg.drawImage(canvas_sw,0,0);
 
 
-	ctx.fillStyle = "#" + (c2 || "FFF");
-	ctx.font = "700 42px Panton"
-	ctx.fillText(name || "XXXX",8,120,112);
-	ctx.fillStyle = "#FFF";
-	ctx.globalCompositeOperation = 'destination-over';
-	ctx.fillRect(0,0,128,128);
+	ctx_bg.fillStyle = "#" + (c2 || "FFF");
+	ctx_bg.font = "700 42px Panton"
+	ctx_bg.fillText(name || "XXXX", 8, 120, 112);
+	ctx_bg.fillStyle = "#FFF";
+	ctx_bg.globalCompositeOperation = 'destination-over';
+	ctx_bg.fillRect(0,0,128,128);
 
-	res.status(200).header('Content-Type','image/png').send( await canvas.png );  
+	res.status(200).header('Content-Type','image/png').send( await canvas_bg.png );  
 })
 
 
