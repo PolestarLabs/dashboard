5aedd3c92eecf6279e75cb253e0a4dea98673569
Flicky <flicky@pollux.gg>
2026-02-23 17:47:56 -0300

    new banner

--- api/src/banner.ts
+++ api/src/banner.ts
@@ -1,24 +1,21 @@
 // ANSI helpers
 const R  = (s: string) => `\x1b[31m${s}\x1b[0m`;   // red
+const B  = (s: string) => `\x1b[36m${s}\x1b[0m`;   // cyan
 const M  = (s: string) => `\x1b[35m${s}\x1b[0m`;   // magenta
 const GR = (s: string) => `\x1b[90m${s}\x1b[0m`;   // gray
 
 export function printBanner(hostname: string, port: number): void {
   const text = [
-    R("                                                                "),
-    R("                                                                "),
-    R("  ,ggggggggggg,                                                 "),
-    R(" dP\"\"\"88\"\"\"\"\"\"Y8,      ,dPYb, ,dPYb,                            "),
-    R(" Yb,  88      `8b      IP'`Yb IP'`Yb                            "),
-    R("  `\"  88      ,8P      I8  8I I8  8I                            "),
-    R("      88aaaad8P\"       I8  8' I8  8'                            "),
-    R("      88\"\"\"\"\",ggggg,   I8 dP  I8 dP  gg      gg     ,gg,   ,gg  "),
-    R("      88    dP\"  \"Y8gggI8dP   I8dP   I8      8I    d8\"\"8b,dP\"   "),
-    R("      88   i8'    ,8I  I8P    I8P    I8,    ,8I   dP   ,88\"     "),
-    R("      88  ,d8,   ,d8' ,d8b,_ ,d8b,_ ,d8b,  ,d8b,,dP  ,dP\"Y8,   "),
-    R("      88  P\"Y8888P\"   8P'\"Y888P'\"Y888P'\"Y88P\"`Y88\"  dP\"   \"Y8  "),
-    R("                                                                "),
-    `     v8.0.alpha                              ${GR("Powered by Elysia")}`,
+    B("                                                                "),
+    B("                                                                "),
+    B("ooooooooo.   oooo                          .o.       ooooooooo.   ooooo "),
+    B("`888   `Y88. `888                         .888.      `888   `Y88. `888' "),
+    B(" 888   .d88'  888  oooo    ooo           .8\"888.      888   .d88'  888  "),
+    B(" 888ooo88P'   888   `88b..8P'   ")+R(",o.o,")+B("   .8' `888.     888ooo88P'   888  "),
+    B(" 888          888     Y888'     ")+R("'888'")+B("  .88ooo8888.    888          888  "),
+    B(" 888          888   .o8\"'88b   ")+R("   ¨ ")+B("  .8'     `888.   888          888  "),
+    B("o888o        o888o o88'   888o       o88o     o8888o o888o        o888o "),
+    `     v0.0.1                                  ${GR("Powered by Elysia")}`,
     R("                                                                "),
     M(`> API listening on http://${hostname}:${port}/api                `),
     R("                                                                "),
@@ -26,3 +23,4 @@ export function printBanner(hostname: string, port: number): void {
 
   console.log(text);
 }
+
