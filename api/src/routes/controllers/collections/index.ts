d285ceec0fbec4f27d9752583479e5cf8a06e0aa
Flicky@Neptune <lucasflicky@hotmail.com>
2026-03-04 14:47:40 -0300

    move

--- api/src/routes/controllers/collections/index.ts
+++ api/src/routes/controllers/collections/index.ts
@@ -1,6 +1,6 @@
 import Elysia from "elysia";
-import { itemsRoutes } from "./collections/items";
-import { craftingRoutes } from "./collections/crafting";
+import { itemsRoutes } from "./items";
+import { craftingRoutes } from "./crafting";
 
 // glue together the two sub-route sets under a single app instance
 export const collectionsRoutes = new Elysia({ tags: ["collections"] })
