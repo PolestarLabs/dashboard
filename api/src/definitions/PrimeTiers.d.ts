fc9c801f279a5d873f7e96c800f7d90a6f0b8dcb
Flicky <flicky@pollux.gg>
2026-03-06 07:52:37 -0300

    prime tier definition

---
+++ api/src/definitions/PrimeTiers.d.ts
@@ -0,0 +1,14 @@
+
+export type PrimeTier = "Aluminium" | "Carbon" | "Iridium" | "Lithium" | "Neutrino" | "Platinum" | "Uranium" | "Zircon" | "Astatine";
+export enum PrimeTiersEnum {
+    uranium     = "Uranium",
+    antimatter  = "Antimatter",
+    aluminium   = "Aluminium",
+    plastic     = "Plastic",
+    astatine    = "Astatine",
+    iridium     = "Iridium",
+    zircon      = "Zircon",
+    lithium     = "Lithium",
+    carbon      = "Carbon",
+    neutrino    = "Neutrino"
+};
