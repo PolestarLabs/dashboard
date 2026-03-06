bd517c6225b192176739ce3b4f3785b35bf8394b
Flicky <flicky@pollux.gg>
2026-03-06 07:31:26 -0300

    profilecard fields definition

---
+++ api/src/definitions/Profilecard.d.ts
@@ -0,0 +1,11 @@
+type Profilecard = {
+    background: string;
+    sticker: string | null;
+    color: string;
+    flair: string;
+    about: string;
+    tagline: string;
+    medals: string[];
+};
+
+export default Profilecard;
