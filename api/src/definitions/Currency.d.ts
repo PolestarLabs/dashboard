6936b2a96e32057c874f489db6c489f74676757e
Flicky <flicky@pollux.gg>
2026-03-07 05:17:46 -0300

    update currency def

--- api/src/definitions/Currency.d.ts
+++ api/src/definitions/Currency.d.ts
@@ -1,1 +1,12 @@
-export type Currency = "RBN" | "JDE" | "SPH" | "EVT";
+export enum CurrencyEnum {
+  RBN = "Rubine",
+  JDE = "Jade",
+  SPH = "Sapphire",
+  AMY = "Amethyst",
+  EMD = "Emerald",
+  TPZ = "Topaze",
+  PSM = "Prism",
+  EVT = "Event",
+};
+
+export type Currency = keyof typeof CurrencyEnum;
