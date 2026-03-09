febc9665713e4e1dfa6ec9da8e0aac46eb6c52f6
Flicky <flicky@pollux.gg>
2026-03-09 17:01:54 -0300

    prime info types

--- api/src/definitions/PrimeTiers.d.ts
+++ api/src/definitions/PrimeTiers.d.ts
@@ -1,14 +1,12 @@
 
-export type PrimeTier = "Aluminium" | "Carbon" | "Iridium" | "Lithium" | "Neutrino" | "Platinum" | "Uranium" | "Zircon" | "Astatine";
-export enum PrimeTiersEnum {
-    uranium     = "Uranium",
-    antimatter  = "Antimatter",
-    aluminium   = "Aluminium",
-    plastic     = "Plastic",
-    astatine    = "Astatine",
-    iridium     = "Iridium",
-    zircon      = "Zircon",
-    lithium     = "Lithium",
-    carbon      = "Carbon",
-    neutrino    = "Neutrino"
-};
+export type PrimeTier = 
+    "plastic" 
+    | "aluminium" 
+    | "carbon" | "iron"
+    | "iridium"| "lithium" 
+    | "palladium" 
+    | "zircon" 
+    | "uranium"
+    | "astatine"
+    | "antimatter" 
+    | "neutrino" 
